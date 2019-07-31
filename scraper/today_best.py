de18438f215d6ce440ff73aa89a0d550e146eb37
Soochul Bang <scbang@lunit.io>
2019-07-31 14:20:49 +0900

    제목에 <>이 포함된 경우 대응

--- scraper/today_best.py
+++ scraper/today_best.py
@@ -110,8 +110,8 @@ def _get_list(query_obj, date_obj):
             genre = re.split("[\[\]]", title_element.strong.text.strip())[1]
             td_list[2].a.strong.decompose()
             titles = re.split("[<>]", title_element.text.strip())
-            title = titles[0].strip()
-            episode = int(titles[1].strip().split("편")[0])
+            title = titles[1].strip()
+            episode = int(titles[len(titles) - 2].strip().split("편")[0])
 
             if episode_limit and episode_limit < episode:
                 continue
