418b1529ac019cfbca638d68c80324f1c5491e99
Soochul Bang <scbang@lunit.io>
2019-08-02 11:50:48 +0900

    스타일 추가시 버그 수정

--- main.py
+++ main.py
@@ -44,26 +44,28 @@ def main(date_str, file_name):
     sheet_rows = sheet_rows + scraper.today_best.get_free_list(date_obj)
     sheet_rows = sheet_rows + scraper.today_best.get_lately_list(date_obj)
 
-    over_10k_favorite_color = BLUE
-    over_10k_favorite_bg_color = "F0E8DD"
+    if "over_10k_style" not in xlsx_obj.style_names:
+        over_10k_favorite_color = BLUE
+        over_10k_favorite_bg_color = "F0E8DD"
 
-    over_20k_favorite_color = RED
-    over_20k_favorite_bg_color = "D1F5EC"
+        over_10k_style = NamedStyle(name="over_10k_style")
+        over_10k_style.font = Font(color=over_10k_favorite_color)
+        over_10k_style.fill = PatternFill(start_color=over_10k_favorite_bg_color,
+                                          end_color=over_10k_favorite_bg_color,
+                                          fill_type="solid")
 
-    over_10k_style = NamedStyle(name="over_10k_style")
-    over_10k_style.font = Font(color=over_10k_favorite_color)
-    over_10k_style.fill = PatternFill(start_color=over_10k_favorite_bg_color,
-                                      end_color=over_10k_favorite_bg_color,
-                                      fill_type="solid")
+        xlsx_obj.add_named_style(over_10k_style)
+    if "over_20k_style" not in xlsx_obj.style_names:
+        over_20k_favorite_color = RED
+        over_20k_favorite_bg_color = "D1F5EC"
 
-    over_20k_style = NamedStyle(name="over_20k_style")
-    over_20k_style.font = Font(color=over_20k_favorite_color)
-    over_20k_style.fill = PatternFill(start_color=over_20k_favorite_bg_color,
-                                      end_color=over_20k_favorite_bg_color,
-                                      fill_type="solid")
+        over_20k_style = NamedStyle(name="over_20k_style")
+        over_20k_style.font = Font(color=over_20k_favorite_color)
+        over_20k_style.fill = PatternFill(start_color=over_20k_favorite_bg_color,
+                                          end_color=over_20k_favorite_bg_color,
+                                          fill_type="solid")
 
-    xlsx_obj.add_named_style(over_10k_style)
-    xlsx_obj.add_named_style(over_20k_style)
+        xlsx_obj.add_named_style(over_20k_style)
 
     for row in sheet_rows:
         row_style = None
