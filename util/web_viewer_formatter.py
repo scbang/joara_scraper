e6dbf7f169fc7cbec5f7adee567395004ccc1750
Soochul Bang <scbang@lunit.io>
2019-10-13 15:52:59 +0900

    《 기호로 회상 대화 처리

--- util/web_viewer_formatter.py
+++ util/web_viewer_formatter.py
@@ -1,10 +1,18 @@
-def is_dialog(line):
+def is_normal_dialog(line):
     return line.startswith('“') \
+           or line.startswith('"') \
            or line.startswith('[') \
            or line.startswith('<') \
            or line.startswith("* * *") \
-           or line.startswith('-') \
-           or line.startswith('《')
+           or line.startswith('-')
+
+
+def is_dialog(line):
+    return is_normal_dialog(line) or is_past_dialog(line)
+
+
+def is_past_dialog(line):
+    return line.startswith('《')
 
 
 def is_title(line):
@@ -41,7 +49,7 @@ def reformat_text(input_file_path="./input.txt", output_file_path="./output.txt"
                 output_lines.append("\n")
                 output_lines.append("\n")
                 output_lines.append("\n")
-                output_lines.append("* * *\n")
+                output_lines.append(input_line)
                 output_lines.append("\n")
                 output_lines.append("\n")
                 output_lines.append("\n")
@@ -52,9 +60,9 @@ def reformat_text(input_file_path="./input.txt", output_file_path="./output.txt"
                 output_lines.append("\n")
                 output_lines.append(input_line)
                 prev_text_mode = text_mode
-                text_mode = "dialog"
+                text_mode = "normal_dialog" if is_normal_dialog(input_line) else "past_dialog"
             else:
-                if prev_text_mode in ["normal", "dialog"]:
+                if prev_text_mode in ["normal", "normal_dialog", "past_dialog"]:
                     output_lines.append("\n")
                     output_lines.append("\n")
                     output_lines.append("\n")
@@ -69,7 +77,7 @@ def reformat_text(input_file_path="./input.txt", output_file_path="./output.txt"
         elif text_mode == "title":
             if is_dialog(input_line):  # turn into dialog mode
                 prev_text_mode = text_mode
-                text_mode = "dialog"
+                text_mode = "normal_dialog" if is_normal_dialog(input_line) else "past_dialog"
             else:
                 prev_text_mode = text_mode
                 text_mode = "normal"
@@ -79,16 +87,23 @@ def reformat_text(input_file_path="./input.txt", output_file_path="./output.txt"
                 output_lines.append("\n")
                 output_lines.append("\n")
                 prev_text_mode = text_mode
-                text_mode = "dialog"
+                text_mode = "normal_dialog" if is_normal_dialog(input_line) else "past_dialog"
             else:  # stay in normal mode
                 output_lines.append("\n")
-        elif text_mode == "dialog":
-            if not is_dialog(input_line):  # turn into normal mode
+        elif text_mode == "normal_dialog":
+            if not is_normal_dialog(input_line):  # turn into normal mode
                 output_lines.append("\n")
                 output_lines.append("\n")
                 output_lines.append("\n")
                 prev_text_mode = text_mode
-                text_mode = "normal"
+                text_mode = "normal" if not is_past_dialog(input_line) else "past_dialog"
+        elif text_mode == "past_dialog":
+            if not is_past_dialog(input_line):  # turn into normal mode
+                output_lines.append("\n")
+                output_lines.append("\n")
+                output_lines.append("\n")
+                prev_text_mode = text_mode
+                text_mode = "normal" if not is_normal_dialog(input_line) else "normal_dialog"
         output_lines.append(input_line)
 
     output_f = open(output_file_path, 'w+')
