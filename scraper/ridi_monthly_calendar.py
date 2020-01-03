0a8022c811856e07d8f61c7e164b039b80b072ef
Soochul Bang <scbang@lunit.io>
2020-01-03 20:26:34 +0900

    리디북스 로맨스 신간 캘린더 이벤트 페이지 분석 기능 수정. 단행본과 연재 모두 출력되도록.

--- scraper/ridi_monthly_calendar.py
+++ scraper/ridi_monthly_calendar.py
@@ -1,26 +1,24 @@
-import re
-from urllib import parse
-
 import requests
 from bs4 import BeautifulSoup
 
-import config
 from config.ridi import make_event_url
+
 
 def print_new_book_count_by_day(event_id):
     page = requests.get(make_event_url(event_id))
     page_text = page.text.replace("&#", "#")
     soup = BeautifulSoup(page_text, 'html.parser')
     event_calendar_list = soup.select(".EventCalendar")
-    day_list = event_calendar_list[0].select(".EventCalendar_Publications")
+    for i in [0, 1]:
+        day_list = event_calendar_list[i].select(".EventCalendar_Publications")
 
-    day_count_list = [0] * 31
+        day_count_list = [0] * 31
 
-    for day in day_list:
-        day_number = int(day.select(".EventCalendar_PublicationsDayNumber")[0].text.replace(".", ""))
-        day_count_list[day_number - 1] = len(day.select(".EvenetCalendar_Row"))
-    sum = 0
-    for day_num, day_count in enumerate(day_count_list):
-        print(day_count)
-        sum += day_count
-    print(f"sum = {sum}")
+        for day in day_list:
+            day_number = int(day.select(".EventCalendar_PublicationsDayNumber")[0].text.replace(".", ""))
+            day_count_list[day_number - 1] = len(day.select(".EvenetCalendar_Row"))
+        sum = 0
+        for day_num, day_count in enumerate(day_count_list):
+            print(day_count)
+            sum += day_count
+        print(f"sum = {sum}")
